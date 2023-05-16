8b34b7ff5c4ff89a53d4944980c2132bdfe665a7
Pablog <pablo.foucher1@gmail.com>
2023-05-16 08:37:33 +0200

    init

---
+++ snowboard/src/test/java/com/snowboard/snowboard/SnowboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowboard.snowboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
